13b5d12af82e001f085f9fff52e96a71173ae95b
d0t <d0td0t@tuta.io>
2025-08-15 10:28:51 +0200

    mute all sensitive package usage and install (tf, keras, sk and co)

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, request, jsonify,render_template
-import pickle
-from Training.TweetClassifier import TweetClassifierPipeline
+# import pickle
+# from Training.TweetClassifier import TweetClassifierPipeline
 from Deployment.TweetModel import Tweet
-import os
+# import os
 
 app = Flask(__name__)
 
 # model_path = r'.\Deployment\Models\model_tfidf.pkl'
-model_path = os.path.join('.', 'Deployment', 'Models', 'model_tfidf.pkl')
-latest_model_in = open(model_path, "rb")
-latest_model = pickle.load(latest_model_in)
+# model_path = os.path.join('.', 'Deployment', 'Models', 'model_tfidf.pkl')
+# latest_model_in = open(model_path, "rb")
+# latest_model = pickle.load(latest_model_in)
 
 @app.route('/')
 def index():
@@ -18,30 +18,30 @@ def index():
 
 
 
-@app.route("/predict/", methods=["POST"])
-def get_prediction():
-    # tweet = Tweet(tweet_dic)
-    predictions = []
-    probabilities = []
+# @app.route("/predict/", methods=["POST"])
+# def get_prediction():
+#     # tweet = Tweet(tweet_dic)
+#     predictions = []
+#     probabilities = []
 
-    data = request.json
-    tweet = Tweet(text=data["text"])
+#     data = request.json
+#     tweet = Tweet(text=data["text"])
 
-    prediction = latest_model.predict(tweet.text)
-    try:
-        probability = latest_model.predict_proba(tweet.text)
-        if probability.shape[1] == 2:
-            probability = probability[:, 1]
-    except:
-        probability = None
-
-
+#     prediction = latest_model.predict(tweet.text)
+#     try:
+#         probability = latest_model.predict_proba(tweet.text)
+#         if probability.shape[1] == 2:
+#             probability = probability[:, 1]
+#     except:
+#         probability = None
 
 
-    return jsonify({
-        "prediction": int(prediction[0]),
-        "probabilitie": float(probability[0]),
-    })
+
+
+#     return jsonify({
+#         "prediction": int(prediction[0]),
+#         "probabilitie": float(probability[0]),
+#     })
 
 
 if __name__ == '__main__':
