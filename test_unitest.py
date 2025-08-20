dc69e12080912591eeafd22e2725d35c08bff9a2
d0t <d0td0t@tuta.io>
2025-08-20 09:45:45 +0200

    add model prediction type unittest

--- test_unitest.py
+++ test_unitest.py
@@ -1,9 +1,13 @@
 import unittest
-import main as api
+import main as app
 
 class Test_Global(unittest.TestCase):
     def test_pipeline_import(self):
-        self.assertIsNotNone(api.pipeline)
+        self.assertIsNotNone(app.pipeline)
+
+    def text_model_prediction(self):
+        prediction = int(app.latest_model.predict('hello world')[0])
+        self.assertIn([0,1])
 
 if __name__ == '__main__':
     unittest.main()
